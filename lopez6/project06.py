b1329cca51fac3d5bdefe0186b96706b2791ff3b
musrex <leandro.n.lopez@gmail.com>
2023-02-25 23:22:52 -0500

    project06 class and main function

--- lopez6/project06.py
+++ lopez6/project06.py
@@ -1,10 +1,23 @@
-class Deque:
+class Transports():
+    
     def __init__(self):
         self.items = []
 
-    def add(self):
-        self.items.append(item)
+    # This method adds transports from the east
+    # or, adds elements to the deque from the right
+    def east(self, thing):
+        self.items.append(thing)
+    
+    # This method adds transports from the west
+    # or, adds elements to the deque from the left
+    def west(self, thing):
+        self.items.insert(0,thing)
 
+    
+    def size(self):
+        return len(self.items)
+    #def remove(self):
+    #    return self.items.pop(0)
 
     #def __init__(self):
     #    self.items = []
@@ -12,12 +25,27 @@ class Deque:
     #    return self.items == []
     #def enqueue(self,item):
     #    self.items.append(item)
-    #def dequeue(self):
-    #    return self.items.pop(0)
+    #
     #def front(self):
     #    return self.items[len(self.items)-1]
     #def size(self):
     #    return len(self.items)
+    def display(self):
+        print(self.items)
 
-stack = Deque()
-print(stack)
+def main():
+    mainStation = Transports()
+    
+    answered = False
+    while not answered:
+        eastUnits = input('Do we have tranports from the east? ')
+        westUnits = input('Do we have tranports from the west? ')
+
+        mainStation.east(eastUnits)
+        mainStation.west(westUnits)
+
+        result = mainStation.size()
+
+    stack.display()
+    print(result)
+
