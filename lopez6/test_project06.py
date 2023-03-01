04e1be0ea959e0d715cd6521b3f632613ba3833a
musrex <leandro.n.lopez@gmail.com>
2023-02-28 20:35:26 -0500

    Tidied up project06

--- lopez6/test_project06.py
+++ lopez6/test_project06.py
@@ -6,6 +6,10 @@ import unittest
 class TestProject06(unittest.TestCase):
     
     def testOptions1(self):
+    '''Summary of TESTOPTIONS1 test:
+    This test checks to see if the OPTIONS function
+    returns 
+    '''
         firstValue = options("1")
         secondValue = 1
         self.assertEqual(firstValue,secondValue)
@@ -17,19 +21,18 @@ class TestProject06(unittest.TestCase):
     
     def testEast(self):
         terminal = Terminal()
-        terminal.east(1)
-        terminal.east(2)
-        terminal.east(3)
-        result = terminal.size()
-        self.assertEqual(result, 3)
+        terminal.west(1)
+        terminal.east(2)        
+        result = terminal.items
+        self.assertEqual(result[1], 2)
 
     def testWest(self):
         terminal = Terminal()
         terminal.east(1)
         terminal.east(2)
-        terminal.east(3)
-        result = terminal.size()
-        self.assertEqual(result, 3)
+        terminal.west(3)
+        result = terminal.items
+        self.assertEqual(result[0], 3)
 
     def testAddTransport1(self):
         direction = 1
--- lopez6/transports.py
+++ lopez6/transports.py
@@ -1,34 +1,23 @@
+# This object is a deque. Short for "double-ended queue,"
+# it is a queue that can accept input from either side
 class Terminal():
     
     def __init__(self):
         self.items = []
+        
 
     # This method adds transports from the east
     # or, adds elements to the deque from the right
     def east(self, thing):
-        self.items.append(thing)
+        return self.items.append(thing)
     
     # This method adds transports from the west
     # or, adds elements to the deque from the left
     def west(self, thing):
-        self.items.insert(0,thing)
+        return self.items.insert(0,thing)
 
-    
     def size(self):
         return len(self.items)
-    #def remove(self):
-    #    return self.items.pop(0)
 
-    #def __init__(self):
-    #    self.items = []
-    #def IsEmpty(self):
-    #    return self.items == []
-    #def enqueue(self,item):
-    #    self.items.append(item)
-    #
-    #def front(self):
-    #    return self.items[len(self.items)-1]
-    #def size(self):
-    #    return len(self.items)
     def display(self):
         print(self.items)
