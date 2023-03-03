6ffe728513990046cb3502a8a231caecd03f11f3
musrex <leandro.n.lopez@gmail.com>
2023-03-03 00:08:47 -0500

    sorting algos

--- lopez7/project07.py
+++ lopez7/project07.py
@@ -7,41 +7,58 @@ from insertionSort import *
 
 def mixer(size):
     '''Summary of mixer() function
-    This function takes a in and returns a mi
+    This function takes an int as an argument
+    and returns a randomized list the size of the argument.
+
+    size(INT): An int that determines the size of the list.
+
+    returns: A randomized list the size of the int passed as 
+    an argument.
     '''
     list = []
     for i in range(size):
         list.append(random.randint(1,size))
     return list
 
+# here we create our various main 
+# list sizes. we do this so all 
+# lists of the same size start 
+# with identical values. 
 list5k = mixer(5000)
 list10k = mixer(10000)
 list50k = mixer(50000)
 list100k = mixer(100000)
 
-# list10k = mixer(10000)
-
+# here I create our copy lists 
+# to run our experiments
+# note - I don't go above 50k
+# for bubblesort() because it
+# took too long...
+# ----------------------
+# 5k lists
 list1 = list5k
 list2 = list5k
-list3 = list5k
-
+# 10k lists
+list3 = list10k
 list4 = list10k
-list5 = list10k
-list6 = list10k
+# 50k lists
+list5 = list50k
+list6 = list50k
+# 100k lists
+list7 = list100k
 
-list7 = list50k
-list8 = list50k
-list9 = list50k
 
-list10 = list100k
-list11 = list100k
-
+# the start of our experiment
+# we declare out start time variable...
 start = default_timer()
 bubblesort(list1)
+# and then call a new timer method, and subtract the start time to 
+# get the time it took for the sorting to finish. we can print this
+# elegantly using f strings.
 print(f"Bubble sort on 5k item list time:  {default_timer() - start} secs")
-bubblesort(list4)
+bubblesort(list3)
 print(f"Bubble sort on 10k item list time:  {default_timer() - start} secs")
-bubblesort(list7)
+bubblesort(list5)
 print(f"Bubble sort on 50k item list time:  {default_timer() - start} secs")
 
 print('--------------------------------------------------------------------')
@@ -49,21 +66,9 @@ print('--------------------------------------------------------------------')
 start = default_timer()
 merge_sort(list2)
 print(f"Merge sort on 5k item list speed:  {default_timer() - start} secs")
-merge_sort(list5)
+merge_sort(list4)
 print(f"Merge sort on 10k item list speed:  {default_timer() - start} secs")
-merge_sort(list8)
+merge_sort(list6)
 print(f"Merge sort on 50k item list time:  {default_timer() - start} secs")
-merge_sort(list10)
+merge_sort(list7)
 print(f"Merge sort on 100k item list time:  {default_timer() - start} secs")
-
-print('--------------------------------------------------------------------')
-
-start = default_timer()
-insertion_sort(list3)
-print(f"Insertion sort on 5k item list speed:  {default_timer() - start} secs")
-insertion_sort(list6)
-print(f"Insertion sort on 10k item list speed:  {default_timer() - start} secs")
-insertion_sort(list9)
-print(f"Insertion sort on 50k item list time:  {default_timer() - start} secs")
-insertion_sort(list11)
-print(f"Insertion sort on 100k item list time:  {default_timer() - start} secs")
