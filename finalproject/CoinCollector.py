0bf0e0b570db148d9aee5a9d0eb90e1a10a94868
musrex <96090997+musrex@users.noreply.github.com>
2023-03-09 17:26:06 -0500

    CoinCollector dictionary setup

--- finalproject/CoinCollector.py
+++ finalproject/CoinCollector.py
@@ -24,6 +24,13 @@ class CoinCollector:
             if x == "H":
                 purse["H"] = purse["H"] + 1
             if x == "W":
-                purse["W"] = purse["W"] + 1            
+                purse["W"] = purse["W"] + 1
 
+        pennies = purse["P"] * .01            
+        nickels = purse["N"] * .05
+        dimes = purse["D"] * .10
+        quarters = purse["Q"] * .25
+        half = purse["H"] * .50
+        dollar = purse["W"] * 1.00
+        bal = pennies + nickels + dimes + quarters + half + dollar
         return purse
